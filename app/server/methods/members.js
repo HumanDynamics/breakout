8b403ac7ad1f61b5e797974f182be7574254369a
Dan Calacci <dan@dcalacci.net>
2015-11-02 13:27:28 -0500

    getting meteor hangout ID on the client, meteor versioning

--- app/server/methods/members.js
+++ app/server/methods/members.js
@@ -8,17 +8,21 @@ Meteor.methods({
     // For now, just do findOne:
     'getHangout': function getHangoutId(hangoutId, hangoutUrl, participants) {
         possible_hangout = Hangouts.findOne({'hangout_id': hangoutId});
+        console.log("finding hangout with id: ", hangoutId);
+        console.log("found possible hangout: ", possible_hangout);
         if (possible_hangout) {
-            return possible_hangout;
+            return {'id': possible_hangout._id,
+                    'new': false};
         } else {
             // no existing hangout, let's make another
-            return Hangouts.insert({'hangout_id': hangoutId,
-                                    'url': hangoutUrl,
-                                    'participants': participants,
-                                    'start_time': Date.now(),
-                                    'end_time': null,
-                                    'active': true,
-            });
+            return {'id': Hangouts.insert({'hangout_id': hangoutId,
+                                           'url': hangoutUrl,
+                                           'participants': participants,
+                                           'start_time': Date.now(),
+                                           'end_time': null,
+                                           'active': true,
+                                          }),
+                    'new': true};
         }
     }
 });
--- plugin-app/src/lib.js
+++ plugin-app/src/lib.js
@@ -7,29 +7,45 @@ window.googleLoaded = false;
 
 state['conn'] = new Asteroid(window.meteorURL, true);
 
-gapi.hangout.onApiReady.add(function(eventObj) {
 
-    // get this hangout ID
+// returns a promise that is the meteor ID of this hangout.  If there
+// is no appropriate hangout, meteor will creates a new one and
+// returns that ID.
+// of the form:
+// {'id': id, 'new': boolean}
+function getHangoutId() {
+    // get participants
     var participantIds = _.each(gapi.hangout.getParticipants,
                                 function(p) {
                                     return p.id;
                                 });
 
-    var hangout_id; 
-    state.conn.call('getHangout',
-                    gapi.hangout.getHangoutId(),
-                    gapi.hangout.getHangoutUrl()
-                    participantIds,
-                    function(error, result) {
-                        hangout_id = result;    
-                    });
-    var hangoutId = hangoutRes.result;
-    console.log("[global] got hangout ID:", hangoutId);
-    
-    
+    var hangoutId; 
+    var res = state.conn.call('getHangout',
+                              gapi.hangout.getHangoutId(),
+                              "fillerUrl",
+                              //gapi.hangout.getHangoutUrl(),
+                              participantIds);
+
+    return res.result;
+}
+
+gapi.hangout.onApiReady.add(function(eventObj) {
+
+    // get the hangout ID
+    var hangoutIdPromise = getHangoutId()
+    var hangoutId;
+    hangoutIdPromise.then(function(result) {
+        console.log("got hangout id:", result);
+        hangoutId = result;
+    }, function(error) {
+        console.log("Couldn't get hangout ID: ", error);
+    });
+
+
     gapi.hangout.av.onVolumesChanged.add(
         function(evt) {
-            // TODO
+            console.log("volumes changed...");
         }
     );
 });
