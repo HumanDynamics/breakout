6b84c9f75f328f4550c20da928174aef322c41cc
Dan Calacci <dan@dcalacci.net>
2015-11-13 16:04:48 -0500

    refactoring volume collection data
    - starting volume collection
    - stopping volume collection

--- plugin-app/src/lib.js
+++ plugin-app/src/lib.js
@@ -5,14 +5,19 @@ window.meteorURL = "breakout.media.mit.edu";
 window.state = {};
 window.googleLoaded = false;
 
+// Asteroid initialization
 state['conn'] = new Asteroid(window.meteorURL, true);
 
+// subscribe to talking_history
 state.conn.subscribe('talking_history');
 state['talkingHistoryCollection'] = state.conn.getCollection('talking_history')
 
-
+// subscribe to h_indices
 state.conn.subscribe('h_indices');
-state['herfindahlIndexCollection'] = state.conn.getCollection('h_indices');
+state['hIndexCollection'] = state.conn.getCollection('h_indices');
+
+state['collectingVolumes'] = false; // true if we're currently collecting data
+state['silenceDetector'] = null; // interval ID of the silence detector.
 
 
 
@@ -22,198 +27,62 @@ state['herfindahlIndexCollection'] = state.conn.getCollection('h_indices');
 // of the form:
 // {'id': id, 'isnew': boolean}
 function getHangoutId() {
-    // get participants
-    var participantIds = _.each(gapi.hangout.getParticipants,
-                                function(p) {
-                                    return p.id;
-                                });
-
+    // return if we already have it
+    if ('hangoutId' in state) {
+        return state.hangoutId;
+    }
+    // get participants (GIDs)
+    var participantIds = _.map(gapi.hangout.getParticipants(),
+                               function(p) {
+                                   return p.person.id;
+                               });
     var hangoutId; 
     var res = state.conn.call('getHangout',
                               gapi.hangout.getHangoutId(),
-                              //"fillerUrl",
                               gapi.hangout.getHangoutUrl(),
                               participantIds);
-
     return res.result;
 }
 
+// local buffering for volume data
 
-var volumes = {};
 
-
+//TODO: none of this works, basically :(
+// called after the hangoutID promise has been hit.
 function setupSubscriptions(hangoutId) {
-    console.log("setting up subscriptions...");
-    state['herfindahlRQ'] = state.talkingHistoryCollection.reactiveQuery({});//{'hangout_id': hangoutId});
+    console.log("setting up subscriptions for hangout: ", hangoutId);
+    state['herfindahlRQ'] = state.hIndexCollection.reactiveQuery(function(item) {
+        return item.hangout_id == hangoutId;
+    });
     console.log('hindex for hangout id:', hangoutId, state.herfindahlRQ.result);
 
-    // state.herfindahlRQ.on('change', function(id) {
-    //     state['hIndex'] = state.herfindahlRQ.result;
-    //     console.log("new h-index:", state.hIndex);
-    // });
+    console.log("herfindahl RQ:", state.herfindahlRQ.result);
+
+    state.herfindahlRQ.on("change", function(id) {
+        console.log("herfindahl on changed!");
+//        state['hIndex'] = state.herfindahlRQ.result;
+    });
 }
 
 
 // Events and code for hangout API, after it's ready.
 gapi.hangout.onApiReady.add(function(eventObj) {
-
     // first, get the meteor ID for this hangout
     var hangoutIdPromise = getHangoutId()
     var hangoutId;
     var isHangoutNew;
+
     hangoutIdPromise.then(function(result) {
         console.log("got hangout id:", result);
         hangoutId = result.id;
         state['hangoutId'] = result.id;
         isHangoutNew = result.isnew;
-
-        console.log("setting up subscriptions...");
         setupSubscriptions(hangoutId);
-        
     }, function(error) {
         console.log("Couldn't get hangout ID: ", error);
     });
 
-
-    
-    // volumes
-
-    var volumes = [];
-    var thisParticipant = gapi.hangout.getLocalParticipantId();
-    var talkState = {'talking': false, 'time': null}
-
-    // constants for talking 'algorithm'
-    var MIN_VOLUME = 2; // minimum volume to count
-    // if at least this amount of time happens between a talk signal
-    // and a null signal, they are considered to have stopped talking.
-    var MIN_SILENCE_LENGTH = 1000;
-    // if at least this amount of time happens between a null signal
-    // and a talk signal, they are considered to have started talking.
-    var MIN_TALK_LENGTH = 500;
-
-    // If we get no signal for this amount of time, consider them no
-    // longer talking.
-    var TALK_TIMEOUT = 1500;
-
-
-    // returns the number of ms between index i and the most recent
-    // last talk event
-    // TODO: These two can be abstracted out
-    function timeSinceLastTalk(i) {
-        for (var j = i - 1; j >= 0; j--) {
-            if (volumes[j].vol > MIN_VOLUME) {
-                return volumes[i].timestamp - volumes[j].timestamp;
-            }
-        }
-    }
-
-    // TODO: abstract out
-    function timeSinceLastQuiet(i) {
-        for (var j = i - 1; j >= 0; j--) {
-            if (volumes[j].vol < MIN_VOLUME) {
-                return volumes[i].timestamp - volumes[j].timestamp;
-            }
-        }
-    }
-
-    // returns true if the user has stopped talking.  defines 'stopped
-    // talking' as the user not having a volume event greater than
-    // MIN_VOLUME for over MIN_SILENCE_LENGTH milliseconds.
-    function stoppedTalking() {
-        if (volumes[volumes.length - 1].vol <= MIN_VOLUME) {
-            return timeSinceLastTalk(volumes.length - 1) > MIN_SILENCE_LENGTH;
-        }
-        return false;
-    }
-    
-    function startedTalking() {
-        if (volumes[volumes.length - 1].vol > MIN_VOLUME) {
-            //console.log("over min volume threshold, testing time..");
-            var res = timeSinceLastQuiet(volumes.length - 1) > MIN_TALK_LENGTH;
-            //console.log("time since quiet:", timeSinceLastQuiet(volumes.length - 1));
-            return res;
-        }
-    }
-
-    function insertTalkEvent(startTime, endTime, volumeData) {
-        state.talkingHistoryCollection.insert({
-            'participant_id': thisParticipant,
-            'hangout_id': hangoutId,
-            'start_time': new Date(startTime),
-            'end_time': new Date(endTime),
-            //'volumes': volumeData
-        });
-    }
-
-    // if the current value of `volumes` contains a distinct talk
-    // event, send it to the server and remove it from the list.
-    function checkTalkEvent() {
-        if (volumes.length < 3) {
-            //console.log("no volumes to examine, continuing...");
-            return;
-        }
-        if (talkState.talking) {
-            //console.log("currently talking...");
-            // Talking -> not talking
-            if (stoppedTalking()) {
-                talkState.talking = false;
-                // get the last time talking
-                lastTime = volumes[volumes.length - 1].timestamp - timeSinceLastTalk(volumes.length - 1);
-                // start and end of talking!
-                console.log("new talk event:", talkState.time, lastTime);
-                insertTalkEvent(talkState.time, lastTime, volumes);
-                volumes = [];
-                
-            }
-        } else if (!talkState.talking) {
-            //console.log("not talking...");
-            if (startedTalking()) {
-                //console.log("just started talking!");
-                talkState.time = Date.now();
-                talkState.talking = true;
-            }
-        }
-    }
-
-    gapi.hangout.av.onVolumesChanged.add(
-        function(evt) {
-            //console.log("volumes changed...", evt.volumes);
-            var eventVolumes = evt.volumes;
-            _.each(eventVolumes, function(v, k, l) {
-                if (k == thisParticipant) {
-                    //console.log(v, k, thisParticipant);
-                    var time = Date.now();
-                    volumes.push({'vol': v,
-                                  'timestamp': time});
-                    // console.log(volumes[volumes.length - 1]);
-                    // console.log(volumes.length);
-                }
-                checkTalkEvent();
-                
-            });
-        }
-    );
-
-
-    
-    // setInterval(function() {
-    //     console.log("h-index: ", getHerfindahlIndex(hangoutId));
-    // }, 11000);
-
-    // check if it's been over TALK_TIMEOUT ms since we last got an
-    // event. If so, then they're not talking.
-    setInterval(function() {
-        if (talkState.talking) {
-            var now = Date.now()
-            var lastEventTime = volumes[volumes.length - 1].timestamp;
-            if ((now - lastEventTime) > TALK_TIMEOUT) {
-                console.log("stopped talking...");
-                talkState.talking = false;
-                lastTime = volumes[volumes.length - 1].timestamp - timeSinceLastTalk(volumes.length - 1);
-                console.log("new talk event (timeout):", talkState.time, lastTime);
-                insertTalkEvent(talkState.time, lastTime, volumes);
-                volumes = [];
-            }
-        }
-    }, 50);
+    // start the volume collector
+    state['collectingVolumes'] = false;
+    startVolumeCollection(hangoutId);
 });
