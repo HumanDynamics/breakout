00640c778919aac3b60ecdcf3370e8ef4a4a4963
Dan Calacci <dan@dcalacci.net>
2016-01-06 11:50:22 -0500

    changed gapi to be imported by requirejs

--- plugin/src/js/main.js
+++ plugin/src/js/main.js
@@ -1,19 +1,30 @@
+// Main Require config.
+// Special cases:
+// Primus: Primus generates its client-side library when the node
+// server runs and makes it available as a REST endpoint. We access that here.
+// Google Hangouts API: we pull this from google's CDN.
 requirejs.config({
     baseUrl: 'https://breakout.media.mit.edu/dev/plugin/src/',
+    // do this to get cross-origin javascript through CDNs, etc.
+    text: {
+        useXhr: function (url, protocol, hostname, port) {
+            // allow cross-domain requests
+            // remote server allows CORS
+            return true;
+        }
+    },
     paths: {
         scripts: './js',
         // charts: './charts/js',
         libs: './bower_components',
         jquery: './bower_components/jquery/dist/jquery',
         d3: './bower_components/d3/d3',
-        underscore: './bower_components/d3/d3',
-        // primus is special, it's generated when the server starts.
-        primus: 'https://breakout.media.mit.edu/primus/primus'
-        // and add others like this...
+        _: './bower_components/underscore/underscore',
+        primus: 'https://breakout.media.mit.edu/primus/primus',
+        gapi: 'https://plus.google.com/hangouts/_/api/v1/hangout'
     }
 });
 
+// Main function / js injection point
 // requirejs(["scripts/lib", "charts/pieChart", "charts/herfindahl", "charts/all"]);
-requirejs(["scripts/lib"], function(lib) {
-    console.log("lib primus: ", lib.primus);
-});
+requirejs(["scripts/lib"]);
